1f72b9744f6162b3146726a0d7d0541e07068a2a
tiffanybrezak <tiffany.elizabeth.b@gmail.com>
2024-12-09 17:08:41 +0100

    feat: mail complete steps

--- mail/script.js
+++ mail/script.js
@@ -11,9 +11,30 @@ NON POTETE UTILIZZARE NESSUN METODO COME:
 - ecc... */
 
 // 1. crea una lista con le email che possono accedere
+const permittedEmails = [
+    "anna@gmail.com",
+    "fred@outlook.com",
+    "luke@yahoo.com",
+    "eliza@msn.com"
+];
 
 // 2. prompt: chiede all'utente la sua email
+const userEmail = prompt("Inserisca la Sua email");
 
-// 3. controlla se la email inserita è presente nella lista (loop)
+// 3. controlla se l'email inserita è presente nella lista (loop)
+let isEmailPermitted = false;
 
-// 4. informa l'utente se può accedere 
+for(let i = 0; i < permittedEmails.length; i++) {
+    const currentEmail = permittedEmails[i];
+    if(currentEmail === userEmail) {
+        isEmailPermitted = true;
+    }
+}
+
+// 4. informa l'utente se può accedere (se l'email è presente nella lista, l'utente può accedere, ALTIRMENTI l'utente non può accedere)
+
+if (isEmailPermitted) {
+    alert("Può accedere")
+} else {
+    alert("Non può accedere")
+};
